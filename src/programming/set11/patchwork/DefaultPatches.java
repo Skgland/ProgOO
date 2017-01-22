af051fc24575dfcb899cd1b2cd76b2ee1002f45c
BB20101997 <blessmann-bennet@t-online.de>
2017-01-22 23:06:28 +0100

    reordered patches, added a patch type, added some comments

--- src/programming/set11/patchwork/DefaultPatches.java
+++ src/programming/set11/patchwork/DefaultPatches.java
@@ -11,7 +11,9 @@ import java.awt.*;
 
 public enum DefaultPatches implements IPatchGenerator {
 
+	//Liek Polygon but only triangles
 	TRIANGLE { //the TRIANGLE version uses different starting points compared to the POLYGON enum
+
 		@Override
 		public void generatePatch(Patch patch) {
 			int   side  = patch.SIDE_LENGTH;
@@ -38,7 +40,9 @@ public enum DefaultPatches implements IPatchGenerator {
 				p2 = p2New;
 			}
 		}
-	}, POLYGON {
+	},
+	//a random polygon inception
+	POLYGON {
 		@Override
 		public void generatePatch(Patch patch) {
 			int side = patch.SIDE_LENGTH;
@@ -75,53 +79,48 @@ public enum DefaultPatches implements IPatchGenerator {
 
 		}
 	},
-
-	CIRCLES_UP_ON_CIRCLES {
-		@Override
-		public void generatePatch(Patch patch) {
-			int   side  = patch.SIDE_LENGTH;
-			GOval gOval;
-			GRect gRect = new GRect(side, side);
-			DefaultPatches.setRandomColor(gRect);
-			patch.add(gRect);
-			for(int i = 10; i >= 1; i--) {
-				for(int x = 0; x < i; x++) {
-					for(int y = 0; y < i; y++) {
-						gOval = new GOval(side / i / 2, side / i / 2);
-						DefaultPatches.setRandomColor(gOval);
-						patch.add(gOval, (x + 0.25) * side / i, (y + 0.25) * side / i);
-					}
-				}
-			}
-		}
-	},
-	SPIRAL_RECTANGLES {
+	//like polygon but with transparency and always the same color
+	POLYGON_TRANS {
 		@Override
 		public void generatePatch(Patch patch) {
 			int side = patch.SIDE_LENGTH;
-			int n    = R_GEN.nextInt(3, 9);
-			n = n >= 5 ? n + 1 : n;//to skip 5 because that would be one of the examples
-			int   width = side / (2 * n + 1);
-			GRect gRect;
-			for(int i = n - 1; i >= 0; i--) {
-				gRect = new GRect(side - width * (i * 2 + 1), width);
-				DefaultPatches.setRandomColor(gRect);
-				patch.add(gRect, i * width, i * width);
 
-				gRect = new GRect(side - width * (i * 2 + 1), width);
-				DefaultPatches.setRandomColor(gRect);
-				patch.add(gRect, (i + 1) * width, side - (i + 1) * width);
+			GRect gRect = new GRect(side, side);
+			DefaultPatches.setRandomColor(gRect);
+			patch.add(gRect);
 
-				gRect = new GRect(width, side - width * (i * 2 + 1));
-				DefaultPatches.setRandomColor(gRect);
-				patch.add(gRect, i * width, (i + 1) * width);
+			GPolygon  gPolygon;
+			final int vertexes = R_GEN.nextInt(5, 10); //<-- can't use four because that's one of the example patches
+			GPoint[]  gPoints  = new GPoint[vertexes];
+			GPoint[]  newGPoints;
+			Color     color    = new Color(R_GEN.nextColor().getRGB() & 0x33FFFFFF, true);
 
-				gRect = new GRect(width, side - width * (i * 2 + 1));
-				DefaultPatches.setRandomColor(gRect);
-				patch.add(gRect, side - (i + 1) * width, i * width);
+			for(int i = 0; i < vertexes; i++) {
+				gPoints[i] = new GPoint((Math.sin(i * 2 * Math.PI / vertexes)) * side / 2, (Math.cos(i * 2 * Math.PI / vertexes)) * side / 2);
 			}
+
+			for(int i = 0; i < vertexes * 1.5; i++) {
+				gPolygon = new GPolygon();
+				for(int p = 0; p < vertexes; p++) {
+					gPolygon.addVertex(gPoints[p].getX() + side / 2, gPoints[p].getY() + side / 2);
+				}
+				gPolygon.setFilled(true);
+				gPolygon.setFillColor(color);
+				gPolygon.setColor(R_GEN.nextColor());
+				patch.add(gPolygon);
+
+				newGPoints = new GPoint[vertexes];
+
+				for(int n = 0; n < vertexes; n++) {
+					newGPoints[n] = new GPoint((gPoints[n].getX() + gPoints[(n + 1) % vertexes].getX()) / 2, (gPoints[n].getY() + gPoints[(n + 1) % vertexes].getY()) / 2);
+				}
+
+				gPoints = newGPoints;
+			}
+
 		}
 	},
+	//like polygon but with different vertex count per polygon
 	MULTI_VERT_POLY {
 		@Override
 		public void generatePatch(Patch patch) {
@@ -156,6 +155,7 @@ public enum DefaultPatches implements IPatchGenerator {
 			}
 		}
 	},
+	//transparent version of MULT_VERT_POLY
 	MULTI_VERT_POLY_TRANS {
 		@Override
 		public void generatePatch(Patch patch) {
@@ -171,8 +171,8 @@ public enum DefaultPatches implements IPatchGenerator {
 			GPoint[] gPoints = new GPoint[]{new GPoint(-radius, -radius), new GPoint(-radius, radius), new GPoint(radius, radius), new GPoint(radius, -radius)}; //init with hole patch size
 
 			double midX, midY;
-			Color color = R_GEN.nextColor();
-			color = new Color(color.getRGB()&0x33FFFFFF,true);
+			Color  color = R_GEN.nextColor();
+			color = new Color(color.getRGB() & 0x33FFFFFF, true);
 
 			//generate inner polygons
 			for(int i = 0; i < 5; i++) {
@@ -191,6 +191,54 @@ public enum DefaultPatches implements IPatchGenerator {
 				gPolygon.setFillColor(color);
 				gPolygon.setColor(R_GEN.nextColor());
 				patch.add(gPolygon);
+			}
+		}
+	},
+	//the name says everything
+	CIRCLES_UP_ON_CIRCLES {
+		@Override
+		public void generatePatch(Patch patch) {
+			int   side  = patch.SIDE_LENGTH;
+			GOval gOval;
+			GRect gRect = new GRect(side, side);
+			DefaultPatches.setRandomColor(gRect);
+			patch.add(gRect);
+			for(int i = 10; i >= 1; i--) {
+				for(int x = 0; x < i; x++) {
+					for(int y = 0; y < i; y++) {
+						gOval = new GOval(side / i / 2, side / i / 2);
+						DefaultPatches.setRandomColor(gOval);
+						patch.add(gOval, (x + 0.25) * side / i, (y + 0.25) * side / i);
+					}
+				}
+			}
+		}
+	},
+	//not realy a spiral but yeah
+	SPIRAL_RECTANGLES {
+		@Override
+		public void generatePatch(Patch patch) {
+			int side = patch.SIDE_LENGTH;
+			int n    = R_GEN.nextInt(3, 9);
+			n = n >= 5 ? n + 1 : n;//to skip 5 because that would be one of the examples
+			int   width = side / (2 * n + 1);
+			GRect gRect;
+			for(int i = n - 1; i >= 0; i--) {
+				gRect = new GRect(side - width * (i * 2 + 1), width);
+				DefaultPatches.setRandomColor(gRect);
+				patch.add(gRect, i * width, i * width);
+
+				gRect = new GRect(side - width * (i * 2 + 1), width);
+				DefaultPatches.setRandomColor(gRect);
+				patch.add(gRect, (i + 1) * width, side - (i + 1) * width);
+
+				gRect = new GRect(width, side - width * (i * 2 + 1));
+				DefaultPatches.setRandomColor(gRect);
+				patch.add(gRect, i * width, (i + 1) * width);
+
+				gRect = new GRect(width, side - width * (i * 2 + 1));
+				DefaultPatches.setRandomColor(gRect);
+				patch.add(gRect, side - (i + 1) * width, i * width);
 			}
 		}
 	};
